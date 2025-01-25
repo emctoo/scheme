2feff7998a3fa0f535e27fe747eb5d3f230dbe68
Xiaogang <maple.hl@gmail.com>
2025-01-25 08:55:00 +0800

    cleanup

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -3,7 +3,7 @@ use crate::interpreter::cps;
 use crate::reader::lexer;
 use crate::reader::parser;
 
-macro_rules! try_or_return_with_error {
+macro_rules! try_or_return_error {
     ($inp:expr) => {
         match $inp {
             Ok(v) => v,
@@ -31,16 +31,16 @@ impl Interpreter {
     }
 
     fn parse(&self, input: &str) -> Result<Vec<parser::Node>, String> {
-        let tokens = try_or_return_with_error!(lexer::tokenize(input));
-        let ast = try_or_return_with_error!(parser::parse(&tokens));
+        let tokens = try_or_return_error!(lexer::tokenize(input));
+        let ast = try_or_return_error!(parser::parse(&tokens));
         Ok(ast)
     }
 
     pub fn execute(&self, input: &str) -> Result<String, String> {
         let parsed = self.parse(input)?;
         match *self {
-            Interpreter::AstWalk(ref i) => Ok(format!("{:?}", try_or_return_with_error!(i.run(&parsed)))),
-            Interpreter::Cps(ref i) => Ok(format!("{:?}", try_or_return_with_error!(i.run(&parsed)))),
+            Interpreter::AstWalk(ref i) => Ok(format!("{:?}", try_or_return_error!(i.run(&parsed)))),
+            Interpreter::Cps(ref i) => Ok(format!("{:?}", try_or_return_error!(i.run(&parsed)))),
         }
     }
 }
